ec3d3d11bb3e5fc34ace279fd299fe57f630dade
Pascal Gélinas <pascal.gelinas@nuecho.com>
2013-05-10 15:46:15 -0400

    Added a basic test case for tree model.

---
+++ src/test/java/com/fasterxml/jackson/javax/json/TreeModelTest.java
@@ -0,0 +1,30 @@
+ /*
+  * Copyright (c) 2002-2013 Nu Echo Inc.  All rights reserved. 
+  */
+
+package com.fasterxml.jackson.javax.json;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+
+import javax.json.*;
+import javax.json.spi.*;
+
+import org.junit.*;
+
+import com.fasterxml.jackson.javax.json.spi.*;
+
+/**
+ * @author Nu Echo Inc.
+ */
+public class TreeModelTest {
+    private static final JsonProvider _provider = new JacksonProvider();
+    @Test
+    public void test() {
+        InputStream in = getClass().getResourceAsStream("object.json");
+        JsonReader reader = _provider.createReader(in);
+        JsonObject object = reader.readObject();
+        assertTrue(object.containsKey("field"));
+    }
+}
